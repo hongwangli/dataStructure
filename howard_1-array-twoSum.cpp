6a55a9500f08dc022b3a358803282f0637a21eae
root <root@localhost.localdomain>
2017-02-28 18:52:31 -0800

    add annotation

--- howard_1-array-twoSum.cpp
+++ howard_1-array-twoSum.cpp
@@ -14,9 +14,9 @@ public:
         map<int,int>::iterator it;
         vector<int> result;
         for (int i = 0; i < nums.size(); i++){
-            it = map_temp.find(target - nums[i]);
+            it = map_temp.find(target - nums[i]);//it 为指针变量，如果找到指向相应位置，没有找到指向最后位置的下一个位置
             if (it != map_temp.end() && i != it->second){
-                result.push_back(min(i+1, it->second +1));
+                result.push_back(min(i+1, it->second +1));// vector push_back 把元素压倒最后一个位置
                 result.push_back(max(i+1, it->second + 1));
                 return result;
             }
